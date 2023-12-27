a5e73b7f5786b7a832647b938518d50837620a91
eyhdev <alaqsa335@gmail.com>
2023-12-27 13:27:11 +0300

    Initial Commit

---
+++ Text/TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextApp.swift
+//  Text
+//
+//  Created by eyh.mac on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
