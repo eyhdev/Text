c5f2b6bdf7f7ab0e842467355515e66ae2a8d3d0
eyhdev <alaqsa335@gmail.com>
2023-12-28 11:38:58 +0300

    Text

--- Text/ContentView.swift
+++ Text/ContentView.swift
@@ -9,16 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+           TextEffect(text: "Hi, I'm EYHAN°")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
--- Text/TextEffect.swift
+++ Text/TextEffect.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct TextEffect: View {
+    
+    let text: String
+    @State private var displayedCharacters = ""
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(displayedCharacters)
+            .font(Font.custom("Bebas Neue", size: 45))
+
+            .bold()
+            .onReceive(timer) { _ in
+                if displayedCharacters.count < text.count {
+                    let index = text.index(text.startIndex, offsetBy: displayedCharacters.count)
+                    displayedCharacters.append(text[index])
+                }
+            }
     }
 }
 
 #Preview {
-    TextEffect()
+    TextEffect(text: "")
 }
