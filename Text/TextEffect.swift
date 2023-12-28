98291cd4aabbeac8d1fa78dd2419163b9f47fb16
eyhdev <alaqsa335@gmail.com>
2023-12-28 11:37:33 +0300

    typewrıter text anımatıon effect

---
+++ Text/TextEffect.swift
@@ -0,0 +1,18 @@
+//
+//  TextEffect.swift
+//  Text
+//
+//  Created by eyh.mac on 27.12.2023.
+//
+
+import SwiftUI
+
+struct TextEffect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextEffect()
+}
